8839580209c478dbf0ac0bf5983759e66e9a2bdd
Carol Calderon <cmcalderonr@unal.edu.co>
2023-09-03 20:25:43 -0400

    fixing details in the app

--- Application/application.py
+++ Application/application.py
@@ -21,19 +21,27 @@ scatter_data = pd.read_csv('scatter_data.csv')
 # Figures
 
 # Fig component 1
+# Define custom category order and labels
+custom_category_order = ['MLE', 'FMLE', 'BTSX']  # Specify the desired order
+custom_category_labels = {'MLE': 'Men', 'FMLE': 'Women', 'BTSX': 'Both'}  # Specify custom labels
+custom_color = 'goldenrod'
 aggregated_data = final_prevalence.groupby('Sex')['Prevalence'].mean().reset_index()
 
 fig = px.bar(
     aggregated_data,
     x='Sex',
     y='Prevalence',
-    color=None,
+    color_discrete_sequence=[custom_color],
     #title='Estimate Average Smoking Prevalence by Sex Category',
+    category_orders={'Sex': custom_category_order},  # Set the custom category order
 )
 fig.update_layout(
     xaxis_title='Sex',
     yaxis_title='Prevalence (%)',
 )
+# Update the category labels on the x-axis
+fig.update_xaxes(categoryorder='array', categoryarray=custom_category_order, title_text='Sex', tickvals=[0, 1, 2], ticktext=[custom_category_labels[category] for category in custom_category_order])
+
 
 # Fig component 2
 # Sort the dataframe by 'TimeDimensionValue' in ascending order
@@ -89,14 +97,19 @@ fig3.update_layout(
 
 # Fig component 4
 # Scatterplot of Smoking Prevalence vs Premature Deaths
+
+# Sort the 'WHO Region' column alphabetically in your scatter_data DataFrame
+scatter_data_sorted = scatter_data.sort_values(by='WHO Region')
+
 fig4 = px.scatter(
-    scatter_data,
+    scatter_data_sorted,
     x='Prevalence',
     y='PrematureDeaths/NCD',
     #title='Relation Smoking Prevalence vs Premature Deaths by NCD (proportion among all NCD)',
     labels={'Prevalence': 'Smoking Prevalence', 'PrematureDeaths/NCD': 'Premature Deaths'},
     animation_frame='Year',
-    color = 'WHO Region' 
+    color = 'WHO Region',
+    hover_name='Country'  
 )
 # Set fixed axis ranges
 fig4.update_xaxes(range=[0, 100])
@@ -120,18 +133,19 @@ app.layout = dbc.Container([
     dbc.Row([
         dbc.Col(
             html.Div([
-                html.H3("Estimate Average Smoking Prevalence by Sex Category"),
+                html.H3("Average Smoking Prevalence by Sex Category"),
                 dcc.Graph(figure=fig),
             ]),
-            width=5  # This column takes half of the row width
+            width=4  # This column takes half of the row width
         ),
         dbc.Col(
             # Component 2 (Right column, first row)
             html.Div([
-                html.H3("Estimate Smoking Prevalence by Country over the years"),
+                html.H3("Smoking Prevalence by Country over the years"),
                 dcc.Graph(figure=fig2),
+                html.P("Prevalence values for each country since 2000 year and the country’s most recent survey, then project to 2025."),
             ]),
-            width=7
+            width=8
         ),
     ]),
 
@@ -139,7 +153,7 @@ app.layout = dbc.Container([
         dbc.Col(
             # Component 3 (Left column, second row)
             html.Div([
-                html.H3("Estimate Average Smoking Prevalence by WHO Region over the time"),
+                html.H3("Average Smoking Prevalence by WHO Region over the time"),
                 dcc.Graph(figure=fig3),
             ]),
             width=6
@@ -155,34 +169,82 @@ app.layout = dbc.Container([
     ]),
 
     dbc.Row([
-        dbc.Col(
-            # Component 5 (Left column, third row)
+    dbc.Col(
+        # Component 5 (Third row, single column)
+        [
+            html.H3('MPOWER Strategy results by country'),
+            html.P("Selected the country:"),
             dcc.Dropdown(
-            id='country-dropdown',
-            options=[{'label': country, 'value': country} for country in final_MPOWER['Country'].unique()],
-            value=final_MPOWER['Country'].iloc[0]
-            )
-        ),
-        
-        dash_table.DataTable(
-            id='score-table',
-            columns=[
-                {'name': 'Year', 'id': 'Year'},
-                {'name': 'M_score', 'id': 'M_score'},
-                {'name': 'P_score', 'id': 'P_score'},
-                {'name': 'O_score', 'id': 'O_score'},
-                {'name': 'W_score', 'id': 'W_score'},
-                {'name': 'E_score', 'id': 'E_score'},
-                {'name': 'R_score', 'id': 'R_score'},
+                id='country-dropdown',
+                options=[{'label': country, 'value': country} for country in final_MPOWER['Country'].unique()],
+                value='Canada'
+            ),
+            
+            dash_table.DataTable(
+                id='score-table',
+                columns = [
+                    {'name': 'Year', 'id': 'Year'},
+                    {'name': 'Monitoring', 'id': 'Monitoring'},
+                    {'name': 'Protecting', 'id': 'Protecting'},
+                    {'name': 'Offering', 'id': 'Offering'},
+                    {'name': 'Warning', 'id': 'Warning'},
+                    {'name': 'Enforcing', 'id': 'Enforcing'},
+                    {'name': 'Raising', 'id': 'Raising'},
                 ],
-        ),
-        dbc.Col(
+            ),
+            html.P("Table Description: This table displays the scores for the selected country."),
+        ],
+        width=6  # Set the width of this column to 6
+    ),
+    
+    dbc.Col(
             # Component 6 (Right column, third row)
-            html.Div("Component 6"),
+            [
+                html.H3('The six MPOWER strategies include:'),
+                html.Div(
+                    dbc.Accordion(
+                        [
+                            dbc.AccordionItem(
+                                "M: Monitor tobacco use and prevention policies",
+                                title="M: Monitor tobacco use and prevention policies",
+                            ),
+                            dbc.AccordionItem(
+                                "P: Protect people from tobacco smoke",
+                                title="P: Protect people from tobacco smoke",
+                            ),
+                            dbc.AccordionItem(
+                                "O: Offer help to quit smoking",
+                                title="O: Offer help to quit smoking",
+                            ),
+                            dbc.AccordionItem(
+                                "W: Warn about the dangers of tobacco",
+                                title="W: Warn about the dangers of tobacco",
+                            ),
+                            dbc.AccordionItem(
+                                "E: Enforce bans on tobacco advertising, promotion and sponsorship",
+                                title="E: Enforce bans on tobacco advertising, promotion and sponsorship",
+                            ),
+                            dbc.AccordionItem(
+                                "R: Raise taxes on tobacco",
+                                title="R: Raise taxes on tobacco",
+                            ),
+                        ],
+                        always_open=True,
+                    )
+                ),
+            ],
             width=6
         ),
     ]),
-], fluid=True)
+    # ...
+], fluid=True, style={'width': '100%', 'height': '110vh'})
+
+
+
+
+
+
+
 
 # Define callback to update the table and graphs based on user input
 
@@ -192,6 +254,20 @@ app.layout = dbc.Container([
 )
 def update_table(selected_country):
     filtered_df = final_MPOWER[final_MPOWER['Country'] == selected_country]
+
+        # Define a mapping from existing column names to desired labels
+    column_label_mapping = {
+        'M_score': 'Monitoring',
+        'P_score': 'Protecting',
+        'O_score': 'Offering',
+        'W_score': 'Warning',
+        'E_score': 'Enforcing',
+        'R_score': 'Raising',
+    }
+
+     # Rename the columns based on the mapping
+    filtered_df.rename(columns=column_label_mapping, inplace=True)
+    
     return filtered_df.to_dict('records')
 
    
